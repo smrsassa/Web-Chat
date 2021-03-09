93157205b5f3379f5c7faaf44941e1ea0f63fa1a
Samuel Mayer <samrufino13@gmail.com>
2021-03-09 14:10:09 -0300

    front-end/area do chat

---
+++ front-end/pages/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <title>WebChat</title>
+
+    <link rel="stylesheet" href="../node_modules/@fortawesome/fontawesome-free/css/all.css">
+    <link rel="stylesheet" href="../src/css/index.css">
+    <link rel="stylesheet" href="../src/css/chat.css">
+</head>
+<body>
+    <section class="wrapperChat">
+        <?php require_once "chatArea.html"; ?>
+    </section>
+</body>
+</html>
