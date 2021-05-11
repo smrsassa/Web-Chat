201665b04cff4e86eb1d21137f819a1ba710c955
Samuel Mayer <samrufino13@gmail.com>
2021-05-11 16:54:51 -0300

    back-end/classe de conexao com a base de dados

---
+++ back-end/class/database.php
@@ -0,0 +1,19 @@
+<?php
+
+class database
+{
+    private const HOST = '127.0.0.1';
+    private const DB = 'webchat';
+    private const USER = 'root';
+    private const PASS = '';
+
+    public $con;
+
+    function __construct()
+    {
+        $this->con = new PDO(
+            'mysql:host='.self::HOST.';dbname='.self::DB.';charset=utf8', self::USER, self::PASS 
+        );
+    }
+   
+}
